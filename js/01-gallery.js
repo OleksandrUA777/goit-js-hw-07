3ab9217f3312f61769e520f117f4b6a97d7b123d
Oleksander Kotik <oleksandergithub@gmail.com>
2022-12-19 16:09:58 +0200

    add files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
+import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
+// Change code below this line
+// 1. Рендер розмітки
+const galleryEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+galleryEl.innerHTML = markup;
+
+galleryEl.addEventListener("click", galleryClickHandler);
+
+function galleryClickHandler(event) {
+  event.preventDefault();
+  // 2. Делегування на gallery
+
+  const imageEl = event.target;
+  if (!imageEl.classList.contains("gallery__image")) {
+    return;
+  }
+  // 3. Робимо фуллскірн фото
+  const originalPhoto = imageEl.dataset.source;
+
+  const fullSceenPhotoMarkup = basicLightbox.create(`
+    <img src=${originalPhoto} width="800" height="600">
+`);
+  // 4. Закриття на esc
+  fullSceenPhotoMarkup.show(() => {
+    window.addEventListener("keydown", EscKeyPressHandler);
+  });
+
+  function EscKeyPressHandler(event) {
+    if (event.code === "Escape") {
+      fullSceenPhotoMarkup.close();
+      window.removeEventListener("keydown", EscKeyPressHandler);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// console.log(SimpleLightbox);
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+
+const markup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`
+  )
+  .join("");
+galleryEl.innerHTML = markup;
+
+galleryEl.addEventListener("click", galleryClickHandler);
+
+function galleryClickHandler(event) {
+  event.preventDefault();
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
